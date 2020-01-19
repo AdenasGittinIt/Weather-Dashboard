9a9d3c60f73efdd14b85cd85f3c12ef540603c8c
Adena McGibbon <justadena@gmail.com>
2020-01-18 21:21:40 -0700

    successfully filtering results down to five dates

--- assets/script.js
+++ assets/script.js
@@ -4,41 +4,40 @@ $("#search").on("click", function() {
     .val()
     .trim()
     .toLowerCase();
-
+  
   const apiKey = "00604984263164d160d696afed305b97";
-  const apiKey2 = "c42e5117a374851a94e1da6171ce8e3b";
-  // Here we are building the URL we need to query the database
+
+  // Building the URLs needed for the ajax request
   let weatherUrl = `https://api.openweathermap.org/data/2.5/weather?q=${cityInput}&units=imperial&appid=${apiKey}`;
   
   let forecastUrl = `https://api.openweathermap.org/data/2.5/forecast?q=${cityInput},&units=imperial&appid=${apiKey}`;  
   
-  
-  let getUvIndexUrl = () => {
-    let lat = localStorage.getItem("lat")
-    let lon = localStorage.getItem("lon")
-    let uvIndexUrl = `https://api.openweathermap.org/data/2.5/uvi?lat=${lat}&lon=${lon}&appi=${apiKey}`;
-    return uvIndexUrl;
+  let getUviUrl = (lat,lon,apiKey) => {
+ //https://samples.openweathermap.org/data/2.5/uvi?lat=37.75&lon=-122.37&appid=00604984263164d160d696afed305b97
+    let uviUrl = `https://samples.openweathermap.org/data/2.5/uvi?lat=${lat}&lon=${lon}&appi=${apiKey}`;
+    return uviUrl;
   }
 
 
 
-  // Here we run our AJAX call to the OpenWeatherMap API
+  // ajax call for current weather conditions
   $.ajax({
     url: weatherUrl,
     method: "GET"
   }).then(function(res) {
-    // set lat and lon in local storage to use in the UV Index API call
+    // set lat and lon in local storage to use in the UV i URL and API call
     localStorage.setItem("lat", res.coord.lat);
     localStorage.setItem("lon", res.coord.lon);
 
     console.log(res);
     console.log(weatherUrl);
-    // console.log("lattitude: " + res.coord.lat);
-    // console.log("longitude: " + res.coord.lon);
+    console.log("lattitude: " + res.coord.lat);
+    console.log("longitude: " + res.coord.lon);
     console.log("Wind Speed: " + res.wind.speed);
     console.log("Humidity: " + res.main.humidity);
     console.log("Temperature (F): " + res.main.temp);
 
+    $(".current-info").html(`${res.name}  (${moment(res.dt,"X").format("MM/DD/YYYY")} )`)
     // Populate HTML with current conditions
     $(".city").html(`<h4> Weather Details for ${res.name}</h4>`);
     $(".icon").html(`
@@ -49,15 +48,88 @@ $("#search").on("click", function() {
     $(".wind").html(`<h5> Wind Speed: ${res.wind.speed}</h5>`);
     $(".humidity").html(`<h5> Humidity: ${res.main.humidity}%</h5>`);
     $(".temp").html(`<h5> Temperature (F): ${res.main.temp}</h5>`);
+    $.ajax({
+      url: `https://cors-anywhere.herokuapp.com/https://samples.openweathermap.org/data/2.5/uvi?lat=${res.coord.lat}&lon=${res.coord.lon}&appid=00604984263164d160d696afed305b97`,
+      method: "GET"
+    }).then(function(res) {
+      $(".uv").html(`<h5>UV i: ${res.value}`)
+      console.log("this is the UV i: " + res.value);
     });
-  
+    });
+    
+
+    
+
+  // ajax call for five day forecast weather conditions.
   $.ajax({
     url: forecastUrl,
     method: "GET"
   }).then(function(res) {
+    
     console.log("forecast URL: " + forecastUrl);
     console.log(res);
     console.log("temperature: " + res.list[0].main.temp);
+
+    // var forecastArr = res.list
+    $(".five-day").empty();
+    var prevRow = "";
+    var currentRow;
+    var count = 0; 
+    var totalTemp = 0;
+    var totalHumid = 0
+    var countToAverage = 0;
+    const forecastDays = [];
+
+    // const found = forecastArr.find(element => element > 10);
+
+    const filteredForecast = res.list.filter(function(hourly){
+        if(forecastDays.includes(moment(hourly.dt, "X").format("MM/DD/YYYY"))){
+          return false
+        } else {
+          forecastDays.push(moment(hourly.dt, "X").format("MM/DD/YYYY"))
+        }
+    });  
+    console.log(filteredForecast);
+    console.log(forecastDays)
+
+
+
+
+
+
+    // for (let i = 0; i < forecastArr.length; i++) {
+
+    //      currentRow = moment(forecastArr[i].dt,"X").format("MM/DD/YYYY");
+    //      totalTemp = totalTemp + forecastArr[i].main.temp
+    //      console.log(moment(forecastArr[i].dt,"X").format("MM/DD/YYYY"), forecastArr[i].main.temp)
+
+         
+
+    //      if(prevRow != currentRow && count < 5  ) {
+    //        $(".five-day").append(moment(forecastArr[i].dt,"X").format("MM/DD/YYYY"),$("<br>"))//moment(res.dt,"X").format("MM/DD/YYYY")
+    //        $(".five-day").append($(`<img src="http://openweathermap.org/img/wn/${forecastArr[i].weather[0].icon}@2x.png" alt="weather icon">`))
+           
+    //        totalTemp = totalTemp/countToAverage;
+           
+    //        $(".five-day").append($("<br>"),`Aveage Temp: ${totalTemp}`, $("<br>"))
+           
+    //        countToAverage++;
+    //        count++;
+    //        countToAverage = 0
+    //        totalTemp = 0
+          
+    //     }
+        
+    //     prevRow = currentRow;
+    //     // <figure>
+    //     // <img src="http://openweathermap.org/img/wn/${res.weather[0].icon}@2x.png" alt="weather icon">
+    //     // <figcaption>${res.weather[0].description}</figcaption>
+    //     // </figure>`)
+      
+    // }
+
+
+    // need to filter the results so that only elements that are at least 24, 48, 72, 96 and 120 hours from the current date and time are returned.
 
     // $(".city").html(`<h4> Weather Details for ${res.name}</h4>`);
     // $(".icon").html(`
@@ -69,16 +141,11 @@ $("#search").on("click", function() {
     // $(".humidity").html(`<h5> Humidity: ${res.main.humidity}%</h5>`);
     // $(".temp").html(`<h5> Temperature (F): ${res.main.temp}</h5>`);
 
-        // need a function here that will fire off the third and final API call for the UV Index
   });
 
-  $.ajax({
-    url: getUvIndexUrl(),
-    method: "GET"
-  }).then(function(res) {
-    console.log("this is the UV Index URL: " + uvIndexUrl);
-    console.log("this is the UV Index: " + res.value);
-  });
+
+
+  
 });
 
 
--- psuedoCode.js
+++ psuedoCode.js
@@ -50,86 +50,91 @@
 
 // Humidity
 
-let lat = response.coord.lat;
-let lon = response.coord.lon;
-let uvIndexUrl = `api.openweathermap.org/data/2.5/uvi?lat=${lat}&lon=${lon}&appi=${apiKey}`
-  $.ajax({
-    url: localStorage.getItem(uvIndexUrl),
-    method: "GET"
-  }).then(function(response) {
-    console.log(response);
-  });
+// If the time is equal to the current time plus 24 hours.  Push to the empty array.
 
-  $("#search").on("click", function() {
-    event.preventDefault();
-    var cityInput = $("#city-input")
-      .val()
-      .trim()
-      .toLowerCase();
-  
-    const apiKey = "00604984263164d160d696afed305b97";
-    // Here we are building the URL we need to query the database
-  
-    let weatherUrl = `https://api.openweathermap.org/data/2.5/weather?q=${cityInput}&units=imperial&appid=${apiKey}`;
-    let forecastUrl = `https://api.openweathermap.org/data/2.5/forecast?q=${cityInput},&units=imperial&appid=${apiKey}`;  
-    
-  
-  
-  
-    // Here we run our AJAX call to the OpenWeatherMap API
-    $.ajax({
-      url: weatherUrl,
-      method: "GET"
-    }).then(function(res) {
-      // Transfer content to HTML
-      localStorage.setItem("lat", res.coord.lat);
-      localStorage.setItem("lon", res.coord.lon);
-      $(".city").html("<h1>" + res.name + " Weather Details</h1>");
-      $(".wind").text("Wind Speed: " + res.wind.speed);
-      $(".humidity").text("Humidity: " + res.main.humidity);
-      $(".temp").text("Temperature (F) " + res.main.temp);
-      // Log the data in the console as well
-      });
-      
-    $.ajax({
-      url: forecastUrl,
-      method: "GET"
-    }).then(function(res) {
-      console.log("forecast URL: " + forecastUrl);
-      console.log(res);
-      console.log("temperature: " + res.list[0].main.temp);
-    });
-  });
-  
+      // Filtering the envents by event name so that if the API returns an event with the same name, it get's excluded from the eventNames array
+      var eventNames = [];
+      const forcastDays = [];
+      currentTime = Date.now();
 
-  var API = {
-    saveExample: function(example) {
-      return $.ajax({
-        headers: {
-          "Content-Type": "application/json"
-        },
-        type: "POST",
-        url: "api/examples",
-        data: JSON.stringify(example)
+      let filteredForecast = res.list.filter(function(res){
+        if (res.dt > currentTime + 24 || currentTime + 48 || currentTime + 72 || currentTime + 96 || currentTime + 120) {
+          forcastDays.push()
+        }
+        // I need to iterate over each object in the array 
+      })
+
+      var filteredEvents = response._embedded.events.filter(function(event){
+        if(eventNames.includes(event.name)) {
+          return false 
+        } else {
+          eventNames.push(event.name);
+          return true 
+        }
       });
-    },
-    getExamples: function() {
-      return $.ajax({
-        url: "api/examples",
-        type: "GET"
-      });
-    },
-    deleteExample: function(id) {
-      return $.ajax({
-        url: "api/examples/" + id,
-        type: "DELETE"
-      });
+      console.log(filteredEvents);
+        // Looping through the filteredEvents to get start date, time, event name and image.  Returning at max, 5 results.
+      for(i = 0; i < 5; i++) {
+        var event = filteredEvents[i].name
+        var eventDate = filteredEvents[i].dates.start.localDate
+        var eventTime = filteredEvents[i].dates.start.localTime
+        var foundImage = filteredEvents[i].images.find(function(image) {
+          return image.ratio === "3_2";
+        });
+        // Creating new HTML elements with my seaxrch results and adding them to the page
+        var eventImageUrl = foundImage.url;
+        var newLink = $("<a>").attr({
+          href: filteredEvents[i].url,
+          target: "_blank"});
+        var newImage = $("<img>").attr("src", eventImageUrl);
+        var newDiv = $("<div>").attr("data-box", "box"+(i+5));
+
+// date and time stuff
+var d = new Date();
+var minutes = d.getMinutes();
+var hour = d.getHours();
+var date = d.getDate();
+var dayIndex = d.getDay();
+var timeNow = hour + ":" + minutes;
+
+console.log(timeNow);
+  
+var days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"]
+
+var today = days[dayIndex];
+
+var monthIndex = d.getMonth();
+var allMonths = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+
+var month = allMonths[monthIndex];
+var year = d.getFullYear();
+
+$("#today").text(today +" " + month +" " + date + ", " + year)
+
+
+
+getCursorRequest.onsuccess = e => {
+  const cursor = e.target.result;
+  if (cursor) {
+    if(cursor.value.status === "in-progress") {
+      const todo = cursor.value;
+      todo.status = "complete";
+      cursor.update(todo);
     }
-  };
+    cursor.continue(); 
+  }
+};
 
-      // $.ajax({
-    //   url: GetUvIndexUrl(),
-    //   method: "GET"
-    // }).then()
-
-  // create a function that will fire off the third ajax call within the .then of the second
+const forecastDays = res.list.filter(function(hourly) {
+  if ((moment(hourly.dt, "X").format("MM/DD/YYYY")) === forecastDays.date) {
+    forecastDays.push(hourly.dt)
+    // forecastDays.push({
+    //   date: moment(hourly.dt, "X").format("MM/DD/YYYY"),
+    //   temp: hourly.main.temp,
+    //   humidity: hourly.main.humidity,
+    //   iconUrl: `http://openweathermap.org/img/wn/${hourly.weather[0].icon}@2x.png`
+    // });
+  } else {
+    return false
+  }
+});
