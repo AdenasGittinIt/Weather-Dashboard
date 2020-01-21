9b8fff17e271fb5f65bd0356be2cc160fe4b120f
Adena McGibbon <justadena@gmail.com>
2020-01-20 19:32:05 -0700

    updated styling, search history now displays

--- assets/script.js
+++ assets/script.js
@@ -1,16 +1,45 @@
+let historyArray = JSON.parse(localStorage.getItem("storedHistory"));
+
+let emptyHistory = [];
+
+
+$().ready(function() {
+  let searchHist = defineHistArray(historyArray,emptyHistory);
+  let lastCity = searchHist.pop();
+  // insert the lastCity as the value of the search text input and then simulate a click to search 
+  if(lastCity !== undefined) {
+    $("#city-input").val(lastCity);
+    $("#search").click();
+    displayCities(searchHist);
+  } else {
+    return  
+  }
+  // insert the last searched city in the search field and bring up current details (so essentially the on click with out a click)
+
+})
+
+
 $("#search").on("click", function() {
   event.preventDefault();
-  var cityInput = $("#city-input")
+  let cityInput = $("#city-input")
     .val()
     .trim()
     .toLowerCase();
-
+  // getting an array to store the city 
+  let searchHist = defineHistArray(historyArray,emptyHistory);
+  
   const apiKey = "00604984263164d160d696afed305b97";
-
+  
   // Building the URLs needed for the ajax request
   let weatherUrl = `https://api.openweathermap.org/data/2.5/weather?q=${cityInput}&units=imperial&appid=${apiKey}`;
-
+  
   let forecastUrl = `https://api.openweathermap.org/data/2.5/forecast?q=${cityInput},&units=imperial&appid=${apiKey}`;
+  
+  //adding city to local storage
+  searchHist.push(cityInput);
+  saveCities(searchHist);
+
+
 
   // ajax call for current weather conditions
   $.ajax({
@@ -32,25 +61,25 @@ $("#search").on("click", function() {
     // Populate HTML with current conditions
 
     $("#current-info").html(
-      `Right now in ${res.name}  (${moment(res.dt, "X").format("MM/DD/YYYY")}) `
+      `${res.name} on ${moment(res.dt, "X").format("ddd MMM D, h:mm a")}`
     );
-    $("#city").html(`<h4> Weather Details:`);
-    $("#icon").html(`
+    // $("#weather-details").html(`<h5> Weather Details: </h5>`);
+    $("#icon").html(` 
     <figure>
     <img src="http://openweathermap.org/img/wn/${res.weather[0].icon}@2x.png" alt="weather icon">
     <figcaption>${res.weather[0].description}</figcaption>
     </figure>`);
-    $("#wind").html(`<h5> Wind Speed: ${res.wind.speed}</h5>`);
-    $("#humidity").html(`<h5> Humidity: ${res.main.humidity}%</h5>`);
-    $("#temp").html(`<h5> Temperature (F): ${res.main.temp}</h5>`);
+    $("#wind").html(`Wind Speed: ${res.wind.speed}`);
+    $("#humidity").html(`Humidity: ${res.main.humidity}%`);
+    $("#temp").html(`Temperature (F): ${res.main.temp}°`);
 
     // ajax call for the UV index. The UV index API is only available to paid accounts
     $.ajax({
       url: `https://cors-anywhere.herokuapp.com/https://samples.openweathermap.org/data/2.5/uvi?lat=${res.coord.lat}&lon=${res.coord.lon}&appid=00604984263164d160d696afed305b97`,
       method: "GET"
     }).then(function(res) {
-      $(".uv").html(`<h5>Fake UV Index: ${res.value}`);
-      console.log("this is the UV i: " + res.value);
+      $("#uv").html(`<h5>Fake UV Index: ${res.value}`);
+      $("#five-day-header").html("Five Day Forecast:")
     });
   });
 
@@ -89,8 +118,6 @@ $("#search").on("click", function() {
       console.log(fiveDayDetails);
     });
     fiveDayDetails.forEach(day => {
-      // ICON DATE TEMP HUMIDITY
-
       let icon = day.inconUrl;
       let date = day.date;
       let temp = day.temp;
@@ -98,19 +125,38 @@ $("#search").on("click", function() {
       let description = day.description;
 
       $("#five-day").append(`
-      <h2 class="header"> ${date}<h2/>
-        <div class="card horizontal">
+      <h6 class="header"> ${date}</h6>
+      <div class="card horizontal">
       <div class="card-image">
         <img src=${icon} alt="weather icon">
       </div>
       <div class="card-stacked">
         <div class="card-content">
-        <p>${description}<p/>
-        <p>Temp: ${temp}<p/>
-      <p>Humidity: ${humidity}<p/>
+        <h6>${description}</h6>
+        <h6>Temp: ${temp}</h6>
+      <h6>Humidity: ${humidity}</h6>
     </div>
    </div>
-  <div/>`);
+  <div/>`)
     });
   });
 });
+
+const defineHistArray = (arr1, arr2) => {
+  if(arr1 !== null) {
+    return arr1
+  } else {
+    return arr2
+  }
+}
+
+const saveCities = (array) => {
+  localStorage.setItem("storedHistory", JSON.stringify(array));
+}
+
+const displayCities = (array) => {
+  array.forEach(city => {
+    let historyList = $("#history-list");
+    historyList.append(`<a href="#" class="collection-item">${city}</a>`);
+  });
+}
--- psuedoCode.js
+++ psuedoCode.js
@@ -1,88 +1,116 @@
-// Use the OpenWeather API to retrieve weather data for cities. The documentation includes a section called "How to start" that will provide basic setup and usage instructions.
-
+// DONE Use the OpenWeather API to retrieve weather data for cities. The documentation includes a section called "How to start" that will provide basic setup and usage instructions.
 
 // DONE Use AJAX to hook into the API to retrieve data in JSON format.
 
-
-// Your app will run in the browser and feature dynamically updated HTML and CSS powered by jQuery.
-
+// DONE Your app will run in the browser and feature dynamically updated HTML and CSS powered by jQuery.
 
 // Display the following under current weather conditions:
 
-
 // DONE City
 
+// DONE Date
 
-// Date
+// DONE Icon image (visual representation of weather conditions)
 
+// DONE Temperature
 
-// Icon image (visual representation of weather conditions)
+// DONE Humidity
 
+// DONE Wind speed
 
-// Temperature
+//DONE-ISH UV index
 
+//DONE  Include a 5-Day Forecast below the current weather conditions. Each day for the 5-Day Forecast should display the following:
 
-// Humidity
+//DONE Date
 
+// DONE Icon image (visual representation of weather conditions)
 
-// Wind speed
+// DONE Temperature
 
+// DONE Humidity
 
-// UV index
+// Application loads last searched city forecast on page load.
 
+//on page load function get searched cities from local storage
+//loops throught the array in local storage and populates the left side of the page with the previously searched cities 
+// i can also set the value="" of the text input to the name of the last searched city
+//an array method that targets the last element in the array 
 
-
-
-// Include a search history so that users can access their past search terms. Clicking on the city name should perform a new search that returns current and future conditions for that city.
-
-
-// Include a 5-Day Forecast below the current weather conditions. Each day for the 5-Day Forecast should display the following:
-
-
-// Date
-
-
-// Icon image (visual representation of weather conditions)
-
-
-// Temperature
+//add to the search on click the set local storage method that pushes the text content of the form field to an empty array
 
 
-// Humidity
+// Application stores previously searched for cities in localstorage and displays them to the user.
 
-// If the time is equal to the current time plus 24 hours.  Push to the empty array.
+// PROBLEM Include a search history so that users can access their past search terms. Clicking on the city name should perform a new search that returns current and future conditions for that city.
 
-scoresArray.forEach(obj => {
-  let initials = obj.initials;
-  let storedScore = obj.score;
-  let resultsP = document.createElement("p");
-  resultsP.innerText = `${initials}: ${storedScore}`;
-  scoresDiv.append(resultsP);
+// IDEA FOR FUTURE DEV If the time is equal to the current time plus 24 hours.  Push to the empty array.
+
+fiveDayDetails.forEach(day => {
+
+
+  let icon = day.inconUrl;
+  let date = day.date;
+  let temp = day.temp;
+  let humidity = day.humidity;
+  let description = day.description;
+
+  $("#five-day").append(`
+  <h2 class="header"> ${date}<h2/>
+    <div class="card horizontal">
+  <div class="card-image">
+    <img src=${icon} alt="weather icon">
+  </div>
+  <div class="card-stacked">
+    <div class="card-content">
+    <p>${description}<p/>
+    <p>Temp: ${temp}<p/>
+  <p>Humidity: ${humidity}<p/>
+</div>
+</div>
+<div/>`);
 });
 
-// let fiveCards = fiveDayDetails.forEach(
-//   makeCard(day)
-// );
-// $("#five-day").append(fiveCards);
-// });
-// });
-// });
+// an array to store high scores
+let emptyArray = [];
 
-// function makeCard (day) {
-// let icon = day.inconUrl;
-// let date = day.date;
-// let temp = day.temp;
-// let humidity = day.humidity;
-// return  `<h2 class="header"> ${date}<h2/>
-// <div class="card horizontal">
-// <div class="card-image">
-// <img src=${icon} alt="weather icon">
-// </div>
-// <div class="card-stacked">
-// <div class="card-content">
-// <p>Temp: ${temp}<p/>
-// <p>Humidity: ${humidity}<p/>
-// </div>
-// </div>
-// <div/>`
-// }
+// the array from local storage
+let storedArray = JSON.parse(window.localStorage.getItem("highScores"));
+
+
+const defineScoresArray = (arr1, arr2) => {
+  if(arr1 !== null) {
+    return arr1
+  } else {
+    return arr2
+  }
+}
+
+postScoreBtn.addEventListener("click", function(event) {
+  event.preventDefault();
+  let scoresArray = defineScoresArray(storedArray,emptyArray)})
+
+  scoresArray.push(userAndScore);
+  saveScores(scoresArray);
+
+  function displayAllScores() {
+    removeEls(timer, startButton, results);
+    let scoresArray = defineScoresArray(storedArray, emptyArray);
+  
+    scoresArray.forEach(obj => {
+      let initials = obj.initials;
+      let storedScore = obj.score;
+      let resultsP = document.createElement("p");
+      resultsP.innerText = `${initials}: ${storedScore}`;
+      scoresDiv.append(resultsP);
+    });
+  }
+
+  const saveScores = (array) => {
+    localStorage.setItem("highScores", JSON.stringify(array));
+  }
+
+  searchHist.forEach(city => {
+    $("#history-list").appendChild(`
+    <a href="#!" class="collection-item">${city}</a>`)
+  })
